21ebecd01de0e3bc262c92e14b226d15d8998474
Mongark <mongarkprogrammer@gmail.com>
2022-01-10 00:57:10 -0400

    :lipstick: Changed movie icon's standard image :lipstick:

--- app/src/main/java/com/ghiblimovies/MainActivity.kt
+++ app/src/main/java/com/ghiblimovies/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ghiblimovies
 import MovieAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -16,12 +17,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var lista : ArrayList<Movie> = ArrayList()
+
+        // Delete after API is implemented
         var testTags: ArrayList<String> = ArrayList<String>()
         testTags.add("9238da")
 
+        var test_icon: ImageView
+        // Delete after API is implemented
+
         recycler = findViewById(R.id.movieRecycler)
 
-        lista.add(Movie(210, "test movie", testTags, "blablabla"))
+        for(i in 0..10) {
+            lista.add(Movie(210, "test movie", testTags, "blablabla"))
+        }
 
         showAdapter(lista)
     }
